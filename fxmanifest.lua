7be2aadc13e3349861b7b6cdcf34d2eec5de3644
D3V1L-VMP <amiralinkf@gmail.com>
2026-07-07 10:59:41 +0330

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'config.json'
+}
+
+lua54 'yes'
